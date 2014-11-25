7492038e1c7fd7f6fb7acc287f85eb9b7e3e3500
JohanJohansson <ras@ras-VirtualBox.(none)>
2014-11-25 14:39:50 +0100

    Transformations work

--- src/ir1_tf_broadcaster.cpp
+++ src/ir1_tf_broadcaster.cpp
@@ -14,10 +14,10 @@ int main(int argc, char **argv) {
     ros::Rate loop_rate(20.0);
     while (n.ok()) {
         transform.setOrigin(tf::Vector3(-0.065, 0.06, 0.0));
-        //transform.setRotation(tf::Quaternion(0, 0, M_PI_2, 1));
         tf::Quaternion q;
-        q.setEuler(M_PI_2, 0.0, 0.0);
+        q.setEuler(0.0, 0.0, M_PI_2);
         transform.setRotation(q);
+        //transform.setRotation(tf::Quaternion(0.0, 0.0, 0.0, 1));
 
         br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "robot_center", "sensor1"));
         loop_rate.sleep();
